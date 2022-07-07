0d36f934ac2e99c65f64003d9fb95715699b6120
Никита Басынин <basynin.nikita@ya.ru>
2022-07-07 09:44:10 +0000

    Update dependencies.py

--- dependencies.py
+++ dependencies.py
@@ -3,11 +3,18 @@ import os
 
 if __name__ == "__main__":
     langs = sys.argv[1].split(',')
-    #langs = 'python,go,java'.split(',')
+    #langs = 'python,golang,java'.split(',')
     for lang in langs:
         if lang == 'python':
             Out = os.popen('pip list').read()
             print(Out)
+        elif lang == 'java':
+            pass
+        elif lang == 'ruby':
+            pass
+        elif lang == 'nodejs':
+            pass
+        elif lang == 'go':
+            os.popen("go list -f '{{ join .Deps \"\n\" }}'")
         else:
             print('Unknown lang')
-    print(langs)
