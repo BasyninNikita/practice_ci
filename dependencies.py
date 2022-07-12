1151341ef9dc415cef2f0a78bf2a3d685b600648
BasyninNikita <32190908+BasyninNikita@users.noreply.github.com>
2022-07-12 14:34:00 +0300

    Update dependencies.py

--- dependencies.py
+++ dependencies.py
@@ -2,17 +2,17 @@ import sys
 import os
 
 if __name__ == "__main__":
-    langs = sys.argv[1] #.split(',')
+    langs = sys.argv[1].split(',')
     #langs = 'python,golang,java'.split(',')
     for lang in langs:
-        if lang.casefold() == 'Python':
+        if lang.casefold() == 'python':
             Out = os.popen('pip list').read()
             print(Out)
         elif lang.casefold() == 'java':
             os.popen('gradle init')
             Out = os.popen('gradle dependencies').read()
             print(Out)
-        elif lang.casefold() == 'nodejs' or lang.casefold() == 'JavaScript':
+        elif lang.casefold() == 'nodejs' or lang.casefold() == 'javascript':
             Out = os.popen('npm list').read()
             print(Out)
         elif lang.casefold() == 'go':
