2024de0db086d19b4808256fe5fa791f248233ec
BasyninNikita <32190908+BasyninNikita@users.noreply.github.com>
2022-07-12 14:20:48 +0300

    Update dependencies.py

--- dependencies.py
+++ dependencies.py
@@ -5,17 +5,17 @@ if __name__ == "__main__":
     langs = sys.argv[1] #.split(',')
     #langs = 'python,golang,java'.split(',')
     for lang in langs:
-        if lang == 'python':
+        if lang.casefold() == 'python':
             Out = os.popen('pip list').read()
             print(Out)
-        elif lang == 'java':
+        elif lang.casefold() == 'java':
             os.popen('gradle init')
             Out = os.popen('gradle dependencies').read()
             print(Out)
-        elif lang == 'nodejs' or lang == 'javascript':
+        elif lang.casefold() == 'nodejs' or lang.casefold() == 'javascript':
             Out = os.popen('npm list').read()
             print(Out)
-        elif lang == 'go':
+        elif lang.casefold() == 'go':
             Out = os.popen("go list -f '{{ join .Deps \"\n\" }}'")
             print(Out)
         else:
